56661f6d6e361a2b0de761c2a6bf8d3c51415beb
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-03-31 07:59:15 -0500

    adding insta meeting

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,23 @@
-import { ReactNode } from "react"
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "ZOOM clone with educationals purposes",
+  icons: {
+    icon: '/icon/logo.svg'
+  }
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
+      <StreamVideoProvider>
         {children}
+      </StreamVideoProvider>
     </main>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
